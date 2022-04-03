96ef4f34a999d2173e42742b440350ac0d33c5fa
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-04-04 05:45:59 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -527,7 +527,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                     "⪻", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "Kembali", data="{}_close({})".format(prefix, modulo_page)
+                    "ʙᴀᴄᴋ", data="{}_close({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
                     "⪼", data="{}_next({})".format(prefix, modulo_page)
