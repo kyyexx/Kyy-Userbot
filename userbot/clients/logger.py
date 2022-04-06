a764e79d05eaf72a21485b0926ca1aeea4e2d0b1
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-04-06 22:44:37 +0700

    Update logger.py

--- userbot/clients/logger.py
+++ userbot/clients/logger.py
@@ -17,7 +17,12 @@ from userbot import KYY2, KYY3, KYY4, KYY5, bot, branch, tgbot
 from userbot import checking
 
 MSG_ON = """
-✨**ҡʏʏ-υѕєявσт ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋғᴛɪғᴋᴀɴ**!!\n━━━━━━━━━━━━━━━\n➠ **ᴜsᴇʀʙᴏᴛ ᴠᴇʀsɪᴏɴ** - `3.1.5@Kyy-Userbot`\n━━━━━━━━━━━━━━━\n➠ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :** @NastyProject
+✨**ҡʏʏ-υѕєявσт ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋғᴛɪғᴋᴀɴ**!!
+━━━━━━━━━━━━━━━
+➠ **Userbot Version -** `{}@{}`
+➠ **Ketik** `{}ping` **untuk Mengecheck Bot**
+━━━━━━━━━━━━━━━
+➠ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :** @NastyProject
 """
 
 
