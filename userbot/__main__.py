ba737756e84021db28171f92120f18725767db52
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-04-06 23:50:28 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -31,17 +31,18 @@ except Exception as e:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
+LOGS.info(
+    f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/NastySupportt")
+LOGS.info(
+    f"✨Kyy-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+
+bot.loop.run_until_complete(autobot())
 bot.loop.run_until_complete(kyy_ubot_on())
 bot.loop.run_until_complete(autopilot())
 if not BOT_TOKEN:
     LOGS.info(
         "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
     )
-    bot.loop.run_until_complete(autobot())
-LOGS.info(
-    f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/NastySupportt")
-LOGS.info(
-    f"✨Kyy-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
