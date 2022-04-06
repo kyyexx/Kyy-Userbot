19ba457139a1322edb6378ee1cb7c146b13f2d2a
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-04-07 00:05:35 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,6 +12,7 @@ from pytgcalls import idle
 from userbot import (
     BOT_VER,
     LOGS,
+    LOOP,
     bot,
 )
 from userbot.modules import ALL_MODULES
@@ -30,23 +31,22 @@ except Exception as e:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
+LOOP.run_until_complete(kyy_ubot_on())
 if not BOTLOG_CHATID:
     LOGS.info(
         "BOTLOG_CHATID Vars tidak terisi, Memulai Membuat Grup Otomatis..."
     )
-    bot.loop.run_until_complete(autopilot())
+    LOOP.run_until_complete(autopilot())
 
+if not BOT_TOKEN:
+    LOGS.info(
+        "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
+    )
+    LOOP.run_until_complete(autobot())
 LOGS.info(
     f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/NastySupportt")
 LOGS.info(
     f"✨Kyy-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
-
-
-bot.loop.run_until_complete(kyy_ubot_on())
-bot.loop.run_until_complete(autobot())
-LOGS.info(
-    "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
-)
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
